6f6304c8af2caac497cf3aa883423ab6bc776f8b
sachinlodhi <sachinlodhi8614@gmail.com>
2022-10-25 10:49:21 +0530

    Modules created

--- frame_extract.py
+++ frame_extract.py
@@ -3,11 +3,23 @@ import cv2
 cap = cv2.VideoCapture("video/sample.mp4")
 i = 0
 Frame_Path = 'Frames'
+ctr = 0
+
+
+# Check if camera opened successfully
+if (cap.isOpened()== False):
+    print("Error opening video file")
 while(cap.isOpened()):
     ret, frame = cap.read()
-    if ret == False:
+    if ret == True:
+        cv2.imshow('Frame', frame)
+        ctr+=1
+    if cv2.waitKey(0)  == ord('q'):
         break
-    cv2.imwrite(Frame_Path+"/frame"+str(i)+'.jpg', frame)
-    i+=1
+print(ctr)
+    # if ret == False:
+    #     break
+    # cv2.imwrite(Frame_Path+"/frame"+str(i)+'.jpg', frame)
+    # i+=1
 cap.release()
 cv2.destroyAllWindows()
